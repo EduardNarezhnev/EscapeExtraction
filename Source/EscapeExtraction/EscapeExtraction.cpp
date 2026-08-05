d459bf1cd9a985aa47275e4f68f619ba04377a3e
Eduard Narezhnev <narezhnev.ed@gmail.com>
2026-08-05 16:06:20 +0300

    Initial commit: clean project structure with .gitignore

---
+++ Source/EscapeExtraction/EscapeExtraction.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeExtraction.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeExtraction, "EscapeExtraction" );
